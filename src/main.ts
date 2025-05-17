1c316a687cf0606de7fbe00c204fd1706516dfa3
Siphelele A Maphumulo <siphelelemaphumulo@gmail.com>
2025-05-17 07:33:43 +0200

    Fix base href for GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +9,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// Set base href dynamically to support GitHub Pages subpath
+platformBrowserDynamic([{ provide: APP_BASE_HREF, useValue: '/Hardware/' }])
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
